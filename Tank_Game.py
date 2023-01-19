e468ad3a27771e60b1d3f706261b5a11170c8343
Franksun6666 <91801888+Franksun6666@users.noreply.github.com>
2023-01-19 01:24:08 -0500

    Update Tank_Game.py
    revised functionalities

--- Tank_Game.py
+++ Tank_Game.py
@@ -39,6 +39,7 @@ mixer.music.load("春节序曲.mp3")
 mixer.music.play(-1)
 font = pygame.font.SysFont(None, 32)
 over_font = pygame.font.SysFont(None, 128)
+
 # Player
 playerImg = pygame.image.load('tank player.png')
 playerX = 370
@@ -51,12 +52,12 @@ status = "top"
 enemyImg = pygame.image.load('enemy.png')
 size = (55, 55)
 enemyImg = pygame.transform.scale(enemyImg, size)
-enemyX = 250
-enemyY = 400
+enemyX = 30
+enemyY = 70
 Delta_enemyX = 0
-Delta_enemyY = 0
+Delta_enemyY = -0.08
 enemystatus = "top"
-last = ""
+last = "top"
 stats = ["top","down","left","right"]
 enemies = 0
 
@@ -74,6 +75,20 @@ bulletstatus = "top"
 lastbullet = ""
 collision = 0
 
+# Poop
+poopImg = pygame.image.load('poop.png')
+s = (3, 3)
+size = (40,40)
+poopImg = pygame.transform.scale(poopImg, size)
+poopX = -30
+poopY = -70
+Delta_poopX = 0
+Delta_poopY = 0
+poop_state = "ready"
+switch = True
+poopstatus = ""
+lastpoop = ""
+
 def player(x,y):
     screen.blit(playerImg, (x, y))
 
@@ -85,6 +100,11 @@ def fire_bullet(x, y):
     bullet_state = "fire"
     screen.blit(bulletImg, (x+5, y+5))
 
+def splash_poop(x, y):
+    global poop_state
+    poop_state = "fire"
+    screen.blit(poopImg, (x+5, y+5))
+    
 # Collision Detection Mechanism     
 def isCollision(enemyX, enemyY, bulletX, bulletY):
     distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
@@ -110,6 +130,7 @@ def enemies_left(x,y,collision):
 run = True
 while run:
     screen.blit(background, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -117,7 +138,7 @@ while run:
         # Detection of keyboard pressing to change the position of the player tank
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                Delta_playerY -= 0.05
+                Delta_playerY -= 0.1
                 Delta_playerX = 0
                 if status == "top":
                     playerImg = pygame.transform.rotate(playerImg, 0)
@@ -129,7 +150,7 @@ while run:
                     playerImg = pygame.transform.rotate(playerImg, 180)
                 status = "top"
             elif event.key == pygame.K_DOWN:
-                Delta_playerY += 0.05
+                Delta_playerY += 0.1
                 Delta_playerX = 0
                 if status == "top":
                     playerImg = pygame.transform.rotate(playerImg, 180)
@@ -141,7 +162,7 @@ while run:
                     playerImg = pygame.transform.rotate(playerImg, 0)
                 status = "down"
             elif event.key == pygame.K_RIGHT:
-                Delta_playerX += 0.05
+                Delta_playerX += 0.1
                 Delta_playerY = 0
                 if status == "top":
                     playerImg = pygame.transform.rotate(playerImg, -90)
@@ -153,7 +174,7 @@ while run:
                     playerImg = pygame.transform.rotate(playerImg, 90)
                 status = "right"
             elif event.key == pygame.K_LEFT:
-                Delta_playerX -= 0.05
+                Delta_playerX -= 0.1
                 Delta_playerY = 0
                 if status == "top":
                     playerImg = pygame.transform.rotate(playerImg, 90)
@@ -196,18 +217,62 @@ while run:
     enemyY += Delta_enemyY
     if enemyX <= 0:
         enemyX = 0
+        Delta_enemyX = 0.05
+        Delta_enemyY = 0
+        if last == "top":
+            enemyImg = pygame.transform.rotate(enemyImg, -90)
+        elif last == "right":
+            enemyImg = pygame.transform.rotate(enemyImg, 0)
+        elif last == "left":
+            enemyImg = pygame.transform.rotate(enemyImg, 180)
+        elif last == "down":
+            enemyImg = pygame.transform.rotate(enemyImg, 90)
+        last = "right"
     elif enemyX >= 736:
         enemyX = 735
+        Delta_enemyX = -0.05
+        Delta_enemyY = 0
+        if last == "top":
+            enemyImg = pygame.transform.rotate(enemyImg, 90)
+        elif last == "right":
+            enemyImg = pygame.transform.rotate(enemyImg, 180)
+        elif last == "left":
+            enemyImg = pygame.transform.rotate(enemyImg, 0)
+        elif last == "down":
+            enemyImg = pygame.transform.rotate(enemyImg, -90)
+        last = "left"
     if enemyY <= 10:
         enemyY = 10
+        Delta_enemyY = 0.05
+        Delta_enemyX = 0
+        if last == "top":
+            enemyImg = pygame.transform.rotate(enemyImg, 180)
+        elif last == "right":
+            enemyImg = pygame.transform.rotate(enemyImg, -90)
+        elif last == "left":
+            enemyImg = pygame.transform.rotate(enemyImg, 90)
+        elif last == "down":
+            enemyImg = pygame.transform.rotate(enemyImg, 0)
+        last = "down"
     elif enemyY >= 550:
         enemyY = 550
+        Delta_enemyY = -0.05
+        Delta_enemyX = 0
+        if last == "top":
+            enemyImg = pygame.transform.rotate(enemyImg, 0)
+        elif last == "right":
+            enemyImg = pygame.transform.rotate(enemyImg, 90)
+        elif last == "left":
+            enemyImg = pygame.transform.rotate(enemyImg, -90)
+        elif last == "down":
+            enemyImg = pygame.transform.rotate(enemyImg, 180)
+        last = "top"
     
     # Enemy movement mechanisms
     seconds=(pygame.time.get_ticks())/1000
-    if seconds - record >= random.randint(1,5):
+    if seconds - record >= random.randint(4,6):
         if enemystatus == "top":
-            Delta_enemyY = random.uniform(-0.06,0)
+            Delta_enemyY = -0.05
             Delta_enemyX = 0
             if last == "top":
                 enemyImg = pygame.transform.rotate(enemyImg, 0)
@@ -218,7 +283,7 @@ while run:
             elif last == "down":
                 enemyImg = pygame.transform.rotate(enemyImg, 180)
         elif enemystatus == "down":
-            Delta_enemyY = random.uniform(0,0.07)
+            Delta_enemyY = 0.05
             Delta_enemyX = 0
             if last == "top":
                 enemyImg = pygame.transform.rotate(enemyImg, 180)
@@ -229,7 +294,7 @@ while run:
             elif last == "down":
                 enemyImg = pygame.transform.rotate(enemyImg, 0)
         elif enemystatus == "right":
-            Delta_enemyX = random.uniform(0,0.07)
+            Delta_enemyX = 0.05
             Delta_enemyY = 0
             if last == "top":
                 enemyImg = pygame.transform.rotate(enemyImg, -90)
@@ -240,7 +305,7 @@ while run:
             elif last == "down":
                 enemyImg = pygame.transform.rotate(enemyImg, 90)
         elif enemystatus =="left":
-            Delta_enemyX = random.uniform(-0.07,0)
+            Delta_enemyX = -0.05
             Delta_enemyY = 0
             if last == "top":
                 enemyImg = pygame.transform.rotate(enemyImg, 90)
@@ -253,6 +318,10 @@ while run:
         last = enemystatus
         enemystatus = stats[random.randint(0,3)]
         record = seconds
+        poopX = enemyX
+        poopY = enemyY
+        splash_poop(poopX, poopY)
+        switch = True
 
     # Bullet Movement 
     if bulletY <= 0 or bulletY >= 550 or bulletX <= 0 or bulletX >= 750:
@@ -267,7 +336,7 @@ while run:
             dir = False
         fire_bullet(bulletX, bulletY)
         if bulletstatus == "down":
-            Delta_bulletY = 0.05
+            Delta_bulletY = 0.2
             Delta_bulletX = 0
             if lastbullet == "top" or lastbullet == "" :
                 bulletImg = pygame.transform.rotate(bulletImg, 180)
@@ -279,7 +348,7 @@ while run:
                 bulletImg = pygame.transform.rotate(bulletImg, 0)
             lastbullet = "down"
         elif bulletstatus == "top":
-            Delta_bulletY = -0.05
+            Delta_bulletY = -0.2
             Delta_bulletX = 0
             if lastbullet == "top" or lastbullet == "" :
                 bulletImg = pygame.transform.rotate(bulletImg, 0)
@@ -292,7 +361,7 @@ while run:
             lastbullet = "top"
         elif bulletstatus == "left":
             Delta_bulletY = 0
-            Delta_bulletX = -0.05
+            Delta_bulletX = -0.2
             if lastbullet == "top" or lastbullet == "" :
                 bulletImg = pygame.transform.rotate(bulletImg, 90)
             elif lastbullet == "right":
@@ -304,7 +373,7 @@ while run:
             lastbullet = "left"
         elif bulletstatus == "right":
             Delta_bulletY = 0
-            Delta_bulletX = 0.05
+            Delta_bulletX = 0.2
             if lastbullet == "top" or lastbullet == "" :
                 bulletImg = pygame.transform.rotate(bulletImg, -90)
             elif lastbullet == "right":
@@ -317,27 +386,105 @@ while run:
         bulletX += Delta_bulletX
         bulletY += Delta_bulletY
 
+    
+
+    # Poop Movement 
+    if poopY <= 0 or poopY >= 580 or poopX <= 0 or poopX >= 780:
+        poopY = enemyY
+        poopX = enemyX
+        poop_state = "ready"
+        switch = True
+
+    if poop_state == "fire":
+        if switch:
+            poopstatus = enemystatus
+            switch = False
+        splash_poop(poopX, poopY)
+        if poopstatus == "down":
+            Delta_poopY = random.uniform(0.1,0.015)
+            Delta_poopX = 0
+            if lastpoop == "top" or lastpoop == "" :
+                poopImg = pygame.transform.rotate(poopImg, 180)
+            elif lastpoop == "right":
+                poopImg = pygame.transform.rotate(poopImg, -90)
+            elif lastpoop == "left":
+                poopImg = pygame.transform.rotate(poopImg, 90)
+            elif last == "down":
+                poopImg = pygame.transform.rotate(poopImg, 0)
+            lastpoop = "down"
+        elif poopstatus == "top":
+            Delta_poopY = random.uniform(-0.15,-0.1)
+            Delta_poopX = 0
+            if lastpoop == "top" or lastpoop == "" :
+                poopImg = pygame.transform.rotate(poopImg, 0)
+            elif lastpoop == "right":
+                poopImg = pygame.transform.rotate(poopImg, 90)
+            elif lastpoop == "left":
+                poopImg = pygame.transform.rotate(poopImg, -90)
+            elif last == "down":
+                poopImg = pygame.transform.rotate(poopImg, 180)
+            lastpoop = "top"
+        elif poopstatus == "left":
+            Delta_poopY = 0
+            Delta_poopX = random.uniform(-0.15,-0.1)
+            if lastpoop == "top" or lastpoop == "" :
+                poopImg = pygame.transform.rotate(poopImg, 90)
+            elif lastpoop == "right":
+                poopImg = pygame.transform.rotate(poopImg, 180)
+            elif lastpoop == "left":
+                poopImg = pygame.transform.rotate(poopImg, 0)
+            elif last == "down":
+                poopImg = pygame.transform.rotate(poopImg, -90)
+            lastpoop = "left"
+        elif poopstatus == "right":
+            Delta_poopY = 0
+            Delta_poopX = random.uniform(0.1,0.15)
+            if lastpoop == "top" or lastpoop == "" :
+                poopImg = pygame.transform.rotate(poopImg, -90)
+            elif lastpoop == "right":
+                poopImg = pygame.transform.rotate(poopImg, 0)
+            elif lastpoop == "left":
+                poopImg = pygame.transform.rotate(poopImg, 180)
+            elif last == "down":
+                poopImg = pygame.transform.rotate(poopImg, 90)
+            lastpoop = "right"
+        poopX += Delta_poopX
+        poopY += Delta_poopY
+    
+    # Collision Detection
     bulletcollide = isCollision(enemyX, enemyY, bulletX, bulletY)
     if bulletcollide: 
         explosionSound = mixer.Sound("explosion.wav")
         explosionSound.play()
         enemyX = random.randint(0, 736)
         enemyY = random.randint(50, 150)
+        poopstatus = "top"
+        lastpoop = ""
         collision +=1 
 
     tankscollide = isCollision(playerX, playerY, enemyX, enemyY)
-    if tankscollide:
-        enemyImg = pygame.transform.scale (enemyImg, (0,0))
+    poopcollide = isCollision(playerX, playerY, poopX, poopY)
+    if tankscollide or poopcollide:
+        explosionSound = mixer.Sound("explosion.wav")
+        explosionSound.play()
+        enemyImg = pygame.transform.scale (enemyImg, (0,0)) 
         playerImg = pygame.transform.scale (playerImg, (0,0))
+        poopImg = pygame.transform.scale (poopImg, (0,0))
+        bulletImg = pygame.transform.scale (bulletImg, (0,0))
         game_over_text()
         if seconds - record >= 5:
-            break
+            sys.exit()
+
     if collision >= 5:
         enemyImg = pygame.transform.scale (enemyImg, (0,0)) 
         playerImg = pygame.transform.scale (playerImg, (0,0))
+        poopImg = pygame.transform.scale (poopImg, (0,0))
+        bulletImg = pygame.transform.scale (bulletImg, (0,0))
+        enemies = 0
         win_text()
         if seconds - record >= 5:
-            break
+            sys.exit()
+
     player(playerX,playerY)
     enemy(enemyX,enemyY)
     enemies_left(20,20,collision)
